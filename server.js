36f5b18b1363eeef78f8b78f8442b532a807cfdc
mikelsito <mikelsito@gmail.com>
2018-02-24 11:50:15 -0600

    adding server routes

--- server.js
+++ server.js
@@ -19,6 +19,16 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "html/home.html"));
 });
 
+// Basic route that sends the user first to the AJAX Page
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "html/reserve.html"));
+});
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "html/tables.html"));
+});
+
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
