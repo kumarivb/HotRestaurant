d853dd806d95189d37b1db5b5907bbd862763062
Mike Corbett <srcorbetto@gmail.com>
2018-02-24 11:28:34 -0600

    building data

---
+++ tableData.js
@@ -0,0 +1,16 @@
+var reservations = [
+
+	{
+		name: "Mike",
+		phone: "7633775954",
+		email: "mike@mike.com",
+		uniqueId: 1
+	},
+	{
+		name: "Jeff",
+		phone: "1231231234",
+		email: "mike@mike.com",
+		uniqueId: 1
+	}
+
+]
